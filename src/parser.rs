1f2fb11721e69e96ab78c843299db9bf0818d4d3
sharkdp <davidpeter@web.de>
2019-06-15 21:23:09 +0200

    Add #RGB mode to parser

--- src/parser.rs
+++ src/parser.rs
@@ -3,20 +3,38 @@ use regex::Regex;
 
 use crate::x11colors::{NamedColor, X11_COLORS};
 
+fn hex_to_u8(hex: &str) -> u8 {
+    u8::from_str_radix(hex, 16).unwrap()
+}
+
 pub fn parse_color(color: &str) -> Option<Srgb<u8>> {
-    // RRGGBB
-    let re_rrggbb = Regex::new(r"^#?([[:xdigit:]]{2})([[:xdigit:]]{2})([[:xdigit:]]{2})$").unwrap();
+    let color = color.trim();
 
-    if let Some(caps) = re_rrggbb.captures(color) {
-        let rr = caps.get(1).unwrap().as_str();
-        let gg = caps.get(2).unwrap().as_str();
-        let bb = caps.get(3).unwrap().as_str();
+    // #RRGGBB
+    let re_hex_rrggbb =
+        Regex::new(r"^#?([[:xdigit:]]{2})([[:xdigit:]]{2})([[:xdigit:]]{2})$").unwrap();
 
-        let rr = u8::from_str_radix(rr, 16).unwrap();
-        let gg = u8::from_str_radix(gg, 16).unwrap();
-        let bb = u8::from_str_radix(bb, 16).unwrap();
+    if let Some(caps) = re_hex_rrggbb.captures(color) {
+        let rr = hex_to_u8(caps.get(1).unwrap().as_str());
+        let gg = hex_to_u8(caps.get(2).unwrap().as_str());
+        let bb = hex_to_u8(caps.get(3).unwrap().as_str());
 
         return Some(Srgb::from_components((rr, gg, bb)));
+    }
+
+    // #RGB
+    let re_hex_rgb = Regex::new(r"^#?([[:xdigit:]])([[:xdigit:]])([[:xdigit:]])$").unwrap();
+
+    if let Some(caps) = re_hex_rgb.captures(color) {
+        let r = hex_to_u8(caps.get(1).unwrap().as_str());
+        let g = hex_to_u8(caps.get(2).unwrap().as_str());
+        let b = hex_to_u8(caps.get(3).unwrap().as_str());
+
+        let r = 16 * r + r;
+        let g = 16 * g + g;
+        let b = 16 * b + b;
+
+        return Some(Srgb::from_components((r, g, b)));
     }
 
     for &NamedColor(name, r, g, b) in X11_COLORS.iter() {
