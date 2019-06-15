27b5c03e6d861f3ab196bcfed0126c95fac63d0f
sharkdp <davidpeter@web.de>
2019-06-15 21:48:12 +0200

    Add parsing for rgb(…,…,…)

--- src/parser.rs
+++ src/parser.rs
@@ -3,8 +3,12 @@ use regex::Regex;
 
 use crate::x11colors::{NamedColor, X11_COLORS};
 
-fn hex_to_u8(hex: &str) -> u8 {
+fn hex_to_u8_unsafe(hex: &str) -> u8 {
     u8::from_str_radix(hex, 16).unwrap()
+}
+
+fn dec_to_u8(hex: &str) -> Option<u8> {
+    u8::from_str_radix(hex, 10).ok()
 }
 
 fn rgb(r: u8, g: u8, b: u8) -> Srgb<u8> {
@@ -28,9 +32,9 @@ pub fn parse_color(color: &str) -> Option<Srgb<u8>> {
     .unwrap();
 
     if let Some(caps) = re_hex_rrggbb.captures(color) {
-        let r = hex_to_u8(caps.get(1).unwrap().as_str());
-        let g = hex_to_u8(caps.get(2).unwrap().as_str());
-        let b = hex_to_u8(caps.get(3).unwrap().as_str());
+        let r = hex_to_u8_unsafe(caps.get(1).unwrap().as_str());
+        let g = hex_to_u8_unsafe(caps.get(2).unwrap().as_str());
+        let b = hex_to_u8_unsafe(caps.get(3).unwrap().as_str());
 
         return Some(rgb(r, g, b));
     }
@@ -49,15 +53,67 @@ pub fn parse_color(color: &str) -> Option<Srgb<u8>> {
     .unwrap();
 
     if let Some(caps) = re_hex_rgb.captures(color) {
-        let r = hex_to_u8(caps.get(1).unwrap().as_str());
-        let g = hex_to_u8(caps.get(2).unwrap().as_str());
-        let b = hex_to_u8(caps.get(3).unwrap().as_str());
+        let r = hex_to_u8_unsafe(caps.get(1).unwrap().as_str());
+        let g = hex_to_u8_unsafe(caps.get(2).unwrap().as_str());
+        let b = hex_to_u8_unsafe(caps.get(3).unwrap().as_str());
 
         let r = 16 * r + r;
         let g = 16 * g + g;
         let b = 16 * b + b;
 
         return Some(rgb(r, g, b));
+    }
+
+    // rgb(255,0,119)
+    let re_hex_rgb = Regex::new(
+        r"(?x)
+            ^
+            rgb\(
+                ([0-9]{1,3})
+                ,
+                ([0-9]{1,3})
+                ,
+                ([0-9]{1,3})
+            \)
+            $
+        ",
+    )
+    .unwrap();
+
+    if let Some(caps) = re_hex_rgb.captures(color) {
+        let mr = dec_to_u8(caps.get(1).unwrap().as_str());
+        let mg = dec_to_u8(caps.get(2).unwrap().as_str());
+        let mb = dec_to_u8(caps.get(3).unwrap().as_str());
+
+        match (mr, mg, mb) {
+            (Some(r), Some(g), Some(b)) => return Some(rgb(r, g, b)),
+            _ => {}
+        };
+    }
+
+    // 255,0,119
+    let re_hex_rgb2 = Regex::new(
+        r"(?x)
+            ^
+            ([0-9]{1,3})
+            ,
+            ([0-9]{1,3})
+            ,
+            ([0-9]{1,3})
+            $
+        ",
+    )
+    .unwrap();
+
+    if let Some(caps) = re_hex_rgb2.captures(color) {
+        let mr = dec_to_u8(caps.get(1).unwrap().as_str());
+        let mg = dec_to_u8(caps.get(2).unwrap().as_str());
+        let mb = dec_to_u8(caps.get(3).unwrap().as_str());
+
+        match (mr, mg, mb) {
+            (Some(r), Some(g), Some(b)) => return Some(rgb(r, g, b)),
+            _ => {}
+        };
     }
 
     for &NamedColor(name, r, g, b) in X11_COLORS.iter() {
@@ -70,7 +126,7 @@ pub fn parse_color(color: &str) -> Option<Srgb<u8>> {
 }
 
 #[test]
-fn parse_rrggbb() {
+fn parse_hex_rrggbb() {
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("#ff0077"));
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("#FF0077"));
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("ff0077"));
@@ -84,10 +140,21 @@ fn parse_rrggbb() {
 }
 
 #[test]
-fn parse_rgb() {
+fn parse_hex_rgb() {
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("#f07"));
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("#F07"));
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("f07"));
 
     assert_eq!(None, parse_color("#h03"));
+}
+
+#[test]
+fn parse_rgb() {
+    assert_eq!(Some(rgb(255, 0, 119)), parse_color("rgb(255,0,119)"));
+    assert_eq!(Some(rgb(255, 0, 119)), parse_color("255,0,119"));
+    assert_eq!(Some(rgb(1, 2, 3)), parse_color("1,2,3"));
+
+    assert_eq!(None, parse_color("rgb(256,0,0)"));
+    assert_eq!(None, parse_color("rgb(255,0)"));
+    assert_eq!(None, parse_color("rgb(255,0,0"));
 }
