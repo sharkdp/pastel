8a9f58704c9670a87b2fcbd3d31c3b6e52a6ea8a
sharkdp <davidpeter@web.de>
2019-06-16 13:51:51 +0200

    Add hsl parsing

--- src/cli/parser.rs
+++ src/cli/parser.rs
@@ -11,6 +11,10 @@ fn dec_to_u8(hex: &str) -> Option<u8> {
     u8::from_str_radix(hex, 10).ok()
 }
 
+fn dec_to_i32(hex: &str) -> Option<i32> {
+    i32::from_str_radix(hex, 10).ok()
+}
+
 fn rgb(r: u8, g: u8, b: u8) -> Color {
     Color::from_rgb(r, g, b)
 }
@@ -128,6 +132,46 @@ pub fn parse_color(color: &str) -> Option<Color> {
         }
     }
 
+    // hsl(280,35%,40$)
+    let re_hsl = Regex::new(
+        r"(?x)
+            ^
+            hsl\(
+                \s*
+                ([0-9]{1,3})
+                \s*
+                ,
+                \s*
+                ([0-9]{1,3})
+                %
+                \s*
+                ,
+                \s*
+                ([0-9]{1,3})
+                %
+                \s*
+            \)
+            $
+        ",
+    )
+    .unwrap();
+
+    if let Some(caps) = re_hsl.captures(color) {
+        let mh = dec_to_i32(caps.get(1).unwrap().as_str());
+        let ms = dec_to_i32(caps.get(2).unwrap().as_str());
+        let ml = dec_to_i32(caps.get(3).unwrap().as_str());
+
+        match (mh, ms, ml) {
+            (Some(h), Some(s), Some(l)) => {
+                let h = f64::from(h);
+                let s = f64::from(s) / 100.0;
+                let l = f64::from(l) / 100.0;
+                return Some(Color::from_hsl(h, s, l));
+            }
+            _ => {}
+        };
+    }
+
     None
 }
 
@@ -157,7 +201,10 @@ fn parse_hex_rgb() {
 #[test]
 fn parse_rgb() {
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("rgb(255,0,119)"));
-    assert_eq!(Some(rgb(255, 8, 119)), parse_color("  rgb( 255  ,  8  ,  119 )  "));
+    assert_eq!(
+        Some(rgb(255, 8, 119)),
+        parse_color("  rgb( 255  ,  8  ,  119 )  ")
+    );
     assert_eq!(Some(rgb(255, 0, 119)), parse_color("255,0,119"));
     assert_eq!(Some(rgb(1, 2, 3)), parse_color("1,2,3"));
 
@@ -165,4 +212,21 @@ fn parse_rgb() {
     assert_eq!(None, parse_color("rgb(255,0)"));
     assert_eq!(None, parse_color("rgb(255,0,0"));
     assert_eq!(None, parse_color("rgb (256,0,0)"));
+}
+
+#[test]
+fn parse_hsl() {
+    assert_eq!(
+        Some(Color::from_hsl(280.0, 0.2, 0.5)),
+        parse_color("hsl(280,20%,50%)")
+    );
+    assert_eq!(
+        Some(Color::from_hsl(280.0, 0.2, 0.5)),
+        parse_color("hsl(  280 , 20% , 50%)")
+    );
+
+    assert_eq!(None, parse_color("hsl(280,20%,50)"));
+    assert_eq!(None, parse_color("hsl(280,20,50%)"));
+    assert_eq!(None, parse_color("hsl(280%,20%,50%)"));
+    assert_eq!(None, parse_color("hsl(280,20%)"));
 }
