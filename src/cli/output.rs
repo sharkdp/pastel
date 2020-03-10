ff70dab914a55b09e41fce4e4b5ee6012e9b2259
Roy Ivy III <rivy.dev@gmail.com>
2020-03-10 15:20:58 -0500

    refactor/polish ~ fix `cargo clippy` complaint (redundant_field_name)

--- src/cli/output.rs
+++ src/cli/output.rs
@@ -17,7 +17,7 @@ pub struct Output<'a> {
 impl Output<'_> {
     pub fn new(handle: &mut dyn Write) -> Output {
         Output {
-            handle: handle,
+            handle,
             colors_shown: 0,
         }
     }
